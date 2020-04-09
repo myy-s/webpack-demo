01259c50b5dddde086a085620ce05e58c398c322
myy-s <1054165055@qq.com>
2020-04-09 20:43:54 +0800

    css抽成文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
-var HtmlWebpackPlugin = require("html-webpack-plugin");
-var path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
 
 module.exports = {
   mode: "development",
@@ -15,12 +16,26 @@ module.exports = {
       title: "lalala123",
       template: "src/assets/index.html",
     }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+      chunkFilename: "[id].[contenthash].css",
+      ignoreOrder: false,
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: "../",
+              hmr: process.env.NODE_ENV === "development",
+            },
+          },
+          "css-loader",
+        ],
       },
     ],
   },
