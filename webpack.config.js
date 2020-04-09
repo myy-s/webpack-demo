bb882f3c7c34f8c02a3f4058afa01694bda38baf
myy-s <1054165055@qq.com>
2020-04-09 21:01:08 +0800

    生成环境与开发环境分离1

--- webpack.config.js
+++ webpack.config.js
@@ -3,40 +3,29 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 module.exports = {
-  mode: "development",
-  devServer: {
-    contentBase: "./dist",
-  },
-  entry: "./src/index.js",
-  output: {
-    filename: "index.[contenthash].js",
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "lalala123",
-      template: "src/assets/index.html",
-    }),
-    new MiniCssExtractPlugin({
-      filename: "[name].[contenthash].css",
-      chunkFilename: "[id].[contenthash].css",
-      ignoreOrder: false,
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              publicPath: "../",
-              hmr: process.env.NODE_ENV === "development",
-            },
-          },
-          "css-loader",
-        ],
-      },
+    mode: "development",
+    devServer: {
+        contentBase: "./dist",
+    },
+    entry: "./src/index.js",
+    output: {
+        filename: "index.[contenthash].js",
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "lalala123",
+            template: "src/assets/index.html",
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].[contenthash].css",
+            chunkFilename: "[id].[contenthash].css",
+            ignoreOrder: false,
+        }),
     ],
-  },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: ["style-loader", "css-loader"],
+        }, ],
+    },
 };
---
+++ webpack.config.prod.js
@@ -0,0 +1,41 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  devServer: {
+    contentBase: "./dist",
+  },
+  entry: "./src/index.js",
+  output: {
+    filename: "index.[contenthash].js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "lalala123",
+      template: "src/assets/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+      chunkFilename: "[id].[contenthash].css",
+      ignoreOrder: false,
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: "../",
+            },
+          },
+          "css-loader",
+        ],
+      },
+    ],
+  },
+};
